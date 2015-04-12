af7a2373d05695572ab8577cbd922601febb42f1
Nolawee <nome9478@colorado.edu>
2015-04-11 19:25:33 -0600

    Update Assignment8.h

--- Assignment 8/Assignment8.h
+++ Assignment 8/Assignment8.h
@@ -4,7 +4,7 @@
 //
 //  Created by Nolawee Mengist on 4/6/15.
 //  Copyright (c) 2015 nope. All rights reserved.
-//
+//  Worked with Sami Meharzi
 
 #ifndef Assignment_8_Assignment8_h
 #define Assignment_8_Assignment8_h
